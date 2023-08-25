95a1ec4dc930fc16c9ae70f4c7f615724916fcd8
Prassana Pailwan <83069622+prassana7103@users.noreply.github.com>
2023-08-25 16:35:48 +0530

    acb

--- runner.sh
+++ runner.sh
@@ -11,14 +11,18 @@ export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=true &&
 curl -o actions-runner-linux-x64-2.308.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.308.0/actions-runner-linux-x64-2.308.0.tar.gz && 
 echo "9f994158d49c5af39f57a65bf1438cbae4968aec1e4fec132dd7992ad57c74fa  actions-runner-linux-x64-2.308.0.tar.gz" > checksum.txt
 sudo shasum -a 256 -c checksum.txt
-tar xzf ./actions-runner-linux-x64-2.308.0.tar.gz &&
+tar xzf ./actions-runner-linux-x64-2.308.0.tar.gz
+
 response=$(curl -L \
  -X POST \
  -H "Accept: application/vnd.github.v3+json" \
  -H "Authorization: Bearer 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" \
  -H "X-GitHub-Api-Version: 2022-11-28" \
  "https://api.github.com/repos/prassana7103/Go-API/actions/runners/registration-token")
-token=$(echo "$(response)" | jq -r '.token')
+
+token=$(echo "$response" | jq -r '.token')
 echo "Runner Token: $token"
-./config.sh --url https://github.com/prassana7103/Go-API --token $token &&
-./run.sh &"
+
+./config.sh --url https://github.com/prassana7103/Go-API --token $token
+./run.sh &
+
