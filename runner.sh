e39cf3c7281a49ff041910422ddf1dec23fe57d9
Prassana Pailwan <83069622+prassana7103@users.noreply.github.com>
2023-08-25 18:22:26 +0530

    updated

--- runner.sh
+++ runner.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-cd /home/ec2-user && 
-ls &&
-ssh -o StrictHostKeyChecking=no -i ssh.pem ec2-user@65.1.136.208 "sudo yum install git -y && 
-ls &&
-mkdir actions-runner && cd actions-runner &&
-sudo yum install perl-Digest-SHA -y &&
-sudo yum install libicu -y &&
-export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=true &&
-curl -o actions-runner-linux-x64-2.308.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.308.0/actions-runner-linux-x64-2.308.0.tar.gz && 
-echo "9f994158d49c5af39f57a65bf1438cbae4968aec1e4fec132dd7992ad57c74fa  actions-runner-linux-x64-2.308.0.tar.gz" > checksum.txt
-sudo shasum -a 256 -c checksum.txt
+cd /home/ec2-user 
+ls 
+ssh -o StrictHostKeyChecking=no -i ssh.pem ec2-user@65.1.136.208 "sudo yum install git -y 
+ls 
+mkdir actions-runner && cd actions-runner 
+sudo yum install perl-Digest-SHA -y 
+sudo yum install libicu -y 
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=true 
+curl -o actions-runner-linux-x64-2.308.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.308.0/actions-runner-linux-x64-2.308.0.tar.gz 
 tar xzf ./actions-runner-linux-x64-2.308.0.tar.gz
 
 response=$(curl -L \
