0902f604b9c699af57140a5b0c0b02ffe1e73abf
Prassana Pailwan <83069622+prassana7103@users.noreply.github.com>
2023-08-28 10:56:26 +0530

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -19,9 +19,7 @@ ssh -o StrictHostKeyChecking=no -i ssh.pem ec2-user@65.1.136.208 "
                 -H "Authorization: Bearer $GITLAB_TOKEN" \
                 -H "X-GitHub-Api-Version: 2022-11-28" \
                 "https://api.github.com/repos/prassana7103/Go-API/actions/runners/registration-token" | \
-                jq -r .token)
-            echo $TOKEN
-            token_value=$(cat token.txt)
+                jq -r .token) && env && \
             ./config.sh --url https://github.com/prassana7103/Go-API --token $TOKEN
             ./run.sh &
 "
