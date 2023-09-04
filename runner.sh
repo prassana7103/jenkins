8a8aef9fddd3d81b61c24f8619e3ea91a13513d7
Prassana Pailwan <83069622+prassana7103@users.noreply.github.com>
2023-09-04 19:21:39 +0530

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -2,7 +2,7 @@
 
 cd /home/ec2-user 
 
-ssh -o StrictHostKeyChecking=no -i ssh.pem ec2-user@13.127.23.202 "
+ssh -o StrictHostKeyChecking=no -i ssh.pem ec2-user@43.205.145.115 "
 set -x
 git clone https://github.com/prassana7103/jenkins.git
 cd jenkins 
