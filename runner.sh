59dfe04392c248355e93bc414bc24f623fb087b5
Prassana Pailwan <83069622+prassana7103@users.noreply.github.com>
2023-09-04 11:00:36 +0530

    qws

--- runner.sh
+++ runner.sh
@@ -4,6 +4,7 @@ cd /home/ec2-user
 
 ssh -o StrictHostKeyChecking=no -i ssh.pem ec2-user@13.127.23.202 "
 set -x
+sudo rm -rf actions-runner 
 git clone https://github.com/prassana7103/jenkins.git
 cd jenkins 
 chmod +x abc.sh
